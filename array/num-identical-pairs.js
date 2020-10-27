bbb49d30ee68740289f261296287741d5b53b94d
Rahul Sharma <rahuldsharma@hotmail.co.uk>
2020-10-27 23:11:50 +0000

    num identical pairs

---
+++ array/num-identical-pairs.js
@@ -0,0 +1,48 @@
+/*
+* Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+* */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function numIdenticalPairs(nums) {
+  let pairs = 0;
+
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = 1; j < nums.length; j++) {
+      if(nums[i] === nums[j] && i < j) {
+        pairs += 1;
+      }
+    }
+  }
+
+  return pairs;
+};
